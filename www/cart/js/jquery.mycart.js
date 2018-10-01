0d614836e7ca3566695e7fec687d640798e1feba
joroni <jraymund.niconi@gmail.com>
2018-10-01 12:07:37 +0800

    modal fullscreen

--- www/cart/js/jquery.mycart.js
+++ www/cart/js/jquery.mycart.js
@@ -170,7 +170,7 @@
         '<div class="modal-dialog" role="document">' +
         '<div class="modal-content">' +
         '<div class="modal-header">' +
-        '<h4 class="modal-title" id="myModalLabel"><span class="glyphicon glyphicon-shopping-cart"></span> My Cart</h4>' +
+        '<h4 class="modal-title" id="myModalLabel"><i class="material-icons glyphicon-shopping-cart">shopping_cart</i> My Cart</h4>' +
         '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
         '</div>' +
         '<div class="modal-body">' +
